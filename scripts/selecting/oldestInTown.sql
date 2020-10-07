b9759d96c88c5e7921f60bf0b1a27dc47a206943
Antonio De Rinaldis <antonio.derinaldis@outlook.com>
2020-10-07 21:29:09 +0900

    completed basic requirement for selecting, updating and deleting

--- scripts/selecting/oldestInTown.sql
+++ scripts/selecting/oldestInTown.sql
@@ -1,1 +1,1 @@
-select top 1 * from(select * from students group by town_of_origin order by date_of_birth asc);
+select min(date_of_birth), town_of_origin FROM students GROUP BY town_of_origin;
--- scripts/updating/correctDob.sql
+++ scripts/updating/correctDob.sql
@@ -1,1 +1,1 @@
--- Your SQL here
+UPDATE students SET date_of_birth = date_of_birth + interval '100 years' WHERE date_of_birth < '1910-01-01';
